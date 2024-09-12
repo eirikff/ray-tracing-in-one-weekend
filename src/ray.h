568136e5948d59fc2d8cb5f4c5a13692d70ef944
eirikff <1403257+eirikff@users.noreply.github.com>
2024-09-12 22:32:04 +0200

    Update ray to have color

--- src/ray.h
+++ src/ray.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vector3d.h"
+#include "color.h"
 
 
 namespace RayTracer
@@ -11,17 +12,22 @@ class Ray
 public:
 	Ray() { }
 	Ray(const Vector3d &origin, const Vector3d &direction)
-	: m_origin(origin), m_direction(direction)
+	: m_origin(origin), m_direction(direction.Unit())
+	{ }
+	Ray(const Vector3d &origin, const Vector3d &direction, const RGB &color)
+	: m_origin(origin), m_direction(direction.Unit()), m_color(color)
 	{ }
 
 	const Point3d &Origin() const { return m_origin; }
 	const Vector3d &Direction() const { return m_direction; }
+	const RGB &Color() const { return m_color; }
 
 	Vector3d At(double t) const { return m_origin + t * m_direction; }
 
 private:
 	Point3d m_origin;
 	Vector3d m_direction;
+	RGB m_color;
 
 };
 
