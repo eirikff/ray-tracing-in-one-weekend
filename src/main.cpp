89b1bf70e51002a7345e12dd57e2e841e613fd75
eirikff <1403257+eirikff@users.noreply.github.com>
2024-09-17 17:42:18 +0200

    Add glass ball with thickness

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,14 @@ void Run(size_t image_width)
     auto material_ground = std::make_shared<Lambertian>(RGB{0.8, 0.8, 0.0});
     auto material_center = std::make_shared<Lambertian>(RGB{0.1, 0.2, 0.5});
     auto material_left = std::make_shared<Dielectric>(1.5);
+    auto material_bubble = std::make_shared<Dielectric>(1.0 / 1.5);
     auto material_right = std::make_shared<Metal>(RGB{0.8, 0.6, 0.2}, 1.0);
 
 	HittableList scene;
 	scene.Add(std::make_shared<Sphere>(Point3d{0, -100.5, -1.0}, 100, material_ground));
 	scene.Add(std::make_shared<Sphere>(Point3d{0, 0, -1.2}, 0.5, material_center));
 	scene.Add(std::make_shared<Sphere>(Point3d{-1, 0, -1}, 0.5, material_left));
+	scene.Add(std::make_shared<Sphere>(Point3d{-1, 0, -1}, 0.4, material_bubble));
 	scene.Add(std::make_shared<Sphere>(Point3d{1, 0, -1}, 0.5, material_right));
 
 	Camera cam;
